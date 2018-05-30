798dade94994450b183d84d5048bcf040e16756e
Jay Parikh <jrparikh@ucsc.edu>
2018-05-29 22:20:04 -0700

    Player Movement and Shooting

---
+++ Vedeath/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    // Use this for initialization
+    
+
+    public float VelX = 0f;
+    public float VelY = 5f;
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        rb.velocity = new Vector2(VelX, VelY);
+    }
+    
+}
---
+++ Vedeath/Assets/Scripts/Shooting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject Projectile;
+
+    public float fireSpeed;
+    public float fireRate;
+    //public Transform Player;
+    //public float speed;
+
+    void Update()
+    {
+
+        //transform.rotation = Player.transform.rotation;
+
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > fireRate)
+        {
+            fireRate = Time.time + fireSpeed;
+            Instantiate(Projectile, transform.position, transform.rotation);
+
+        }
+    }
+}
