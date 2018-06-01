38f9a67b6f46c9947c7bfd80bd912e7dd2a0beef
Jonathan Morales <jonmora647@gmail.com>
2018-05-31 20:30:50 -0700

    Instancing Organization

---
+++ Vedeath/Assets/Scripts/Enemy_Instancing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Instancing : MonoBehaviour {
+	public Transform enemyTransform;
+
+	// Use this for initialization
+	void Start () {
+		enemyTransform = gameObject.GetComponent<Transform> ();
+		for (int i = 0; i < 10; i++) {
+			Instantiate(enemyTransform, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
