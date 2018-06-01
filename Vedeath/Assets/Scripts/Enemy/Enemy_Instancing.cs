18641a9ed0d4d765650ea5f3dfe42a83387655a4
Jonathan Morales <jonmora647@gmail.com>
2018-05-31 22:10:03 -0700

    Enemy Instancing Working
    - Enemy instancing
    - Speed changes

--- Vedeath/Assets/Scripts/Enemy/Enemy_Instancing.cs
+++ Vedeath/Assets/Scripts/Enemy/Enemy_Instancing.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Instancing : MonoBehaviour {
-	private Transform enemyTransform;
+	public Transform enemyTransform;
 
 	// Use this for initialization
 	void Start () {
-		enemyTransform = gameObject.GetComponent<Transform> ();
+		//enemyTransform = gameObject.GetComponent<Transform> ();
 		for (int i = 0; i < 10; i++) {
-			Instantiate(enemyTransform, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+			Instantiate(enemyTransform, new Vector3(i * 0.75f - 4, 3, 0), Quaternion.identity);
+			Instantiate (enemyTransform, new Vector3 (i * 0.75f - 4, 2, 0), Quaternion.identity);
 		}
 	}
 	
