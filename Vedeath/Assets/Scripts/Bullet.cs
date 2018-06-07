98fc07da919092d221c2d3c0ad3a83b13b2143a9
Jonathan Morales <jonmora647@gmail.com>
2018-06-07 13:46:36 -0700

    Update?

--- Vedeath/Assets/Scripts/Bullet.cs
+++ Vedeath/Assets/Scripts/Bullet.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
     // Use this for initialization
+	public Camera cam;
+	public Vector3 viewPos;
     public float VelX = 0f;
     public float VelY = 5f;
     Rigidbody2D rb;
@@ -13,11 +15,18 @@ public class Bullet : MonoBehaviour {
     void Start()
     {
         this.rb = GetComponent<Rigidbody2D>();
+		cam = (Camera)FindObjectOfType (typeof(Camera));
     }
 
 
     void Update()
     {
+		viewPos = cam.WorldToViewportPoint(this.gameObject.transform.position);
+
         this.rb.velocity = new Vector2(VelX, VelY);
+
+		if (this.viewPos.y < 0 || this.viewPos.y > 1){
+			Destroy (this.gameObject);
+		}
     }
 }
