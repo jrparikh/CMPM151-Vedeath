9d3ba2a8232538e54ded9825872ceee317a28612
Jonathan Morales <jonmora647@gmail.com>
2018-06-06 17:00:58 -0700

    Better Enemy Movement
    - enemy movement is controlled through the enemy container, their parent object!! :D

--- Vedeath/Assets/Scripts/Enemy/Enemy_Instancing.cs
+++ Vedeath/Assets/Scripts/Enemy/Enemy_Instancing.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy_Instancing : MonoBehaviour {
-	public Transform enemyTransform;
+	public Transform enemyContainer;
+	public GameObject enemy;
+	public static Object ins;
+
+	public float x;
+	public float y;
 
 	// Use this for initialization
 	void Start () {
 		//enemyTransform = gameObject.GetComponent<Transform> ();
-		for (int i = 0; i < 10; i++) {
-			Instantiate(enemyTransform, new Vector3(i * 0.75f - 4, 3, 0), Quaternion.identity);
-			Instantiate (enemyTransform, new Vector3 (i * 0.75f - 4, 2, 0), Quaternion.identity);
+		for (int i = 0; i < 5; i++) {
+			Instantiate(enemy, new Vector3(i * 0.75f, y, 0), Quaternion.identity, enemyContainer);
+			Instantiate(enemy, new Vector3(i * -0.75f, y, 0), Quaternion.identity, enemyContainer);
+
+			//Next Row
+			Instantiate (enemy, new Vector3 (i * 0.75f, y - 1, 0), Quaternion.identity, enemyContainer);
+			Instantiate (enemy, new Vector3 (i * -0.75f, y - 1, 0), Quaternion.identity, enemyContainer);
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
--- Vedeath/Assets/Scripts/Enemy/Enemy_Movement.cs
+++ Vedeath/Assets/Scripts/Enemy/Enemy_Movement.cs
@@ -6,55 +6,75 @@ public class Enemy_Movement: MonoBehaviour {
 	
 	//Rigidbody2D enemyRigidBody;
 	public Vector2 movement;
-	Transform enemyTransform;
+	public Transform enemyTransform;
+	public Transform enemyContainerTransform;
 	public Camera cam;
+
+	private GameObject enemies;
 
 	private float nextMove;
 	public float moveTime;
 	public float movSpeed;
 	public float downSpeed;
 
+	public float leftBoundary;
+	public float rightBoundary;
+
+	public bool turn;
 	private bool leftMove;
 	private bool rightMove;
+	public Vector3 viewPos;
+
+	public bool downTrue = false;
 
 	// Use this for initialization
 	void Start () {
-		//enemyRigidBody = gameObject.GetComponent<Rigidbody2D> ();
-		enemyTransform = gameObject.GetComponent<Transform> ();
 		rightMove = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 viewPos = cam.WorldToViewportPoint(enemyTransform.position);
-		Debug.Log ("Position: " + viewPos);
+		viewPos = cam.WorldToViewportPoint(enemyTransform.position);
 
 		if (Time.time > nextMove) {
 			nextMove = Time.time + moveTime;
 			if (rightMove) {
-				enemyTransform.Translate (Vector3.right * movSpeed);
+				enemyContainerTransform.Translate (Vector3.right * movSpeed);
 			}
 		
 			if (leftMove) {
-				enemyTransform.Translate (Vector3.right * -movSpeed);
+				enemyContainerTransform.Translate (Vector3.right * -movSpeed);
 			}
-
-			Debug.Log ("Left: " + leftMove + ", Right: " + rightMove);
 		}
 
+		//Debug.Log (turn);
 		// Down on Left and Right Movement
-		if (viewPos.x < 0.1f && leftMove && !rightMove){
+		if (viewPos.x < leftBoundary && leftMove && !rightMove){
 			rightMove = true;
 			leftMove = false;
-			enemyTransform.Translate (Vector3.down * downSpeed);
-			Debug.Log ("Down Called");
+			enemyContainerTransform.Translate (Vector3.down * downSpeed);
+			moveTime -= 0.05f;
+
+			downTrue = true;
+
 		}
+		if (downTrue) {
+
+		}
+
+		downTrue = false;
+		//Debug.Log (downTrue);
 		// Down on Right and Left Movement
-		if (viewPos.x > 0.9f && rightMove && !leftMove) {
+		if (viewPos.x > rightBoundary && rightMove && !leftMove) {
 			rightMove = false;
 			leftMove = true;
-			enemyTransform.Translate (Vector3.down * downSpeed);
-			Debug.Log ("Down Called");
+			enemyContainerTransform.Translate (Vector3.down * downSpeed);
+			moveTime -= 0.05f;
+
+			downTrue = true; 
+			//Debug.Log (downTrue);
 		}
+		downTrue = false;
+		//Debug.Log (downTrue);
 	}
 }
